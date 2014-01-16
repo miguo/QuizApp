c5bc95893c5b200d985346cd17c516875f2d5aea
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2014-01-16 08:59:48 +0800

    Version 1.1
    Test radio button

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ $(document).ready(function() {
 		var list = $('input[name=radio-buttons]:checked').parent();
 		//var guesses = $('input[name=radio-buttons]').parent();
 		var valGuess = $('input[name=radio-buttons]:checked').val()
+		var button = $(this input[name='radio-buttons'])
 
 		if (valGuess == 1) {
 			$(this).text('You got it!');
 			$(list).css("background-color", "#2ecc71");
+			$(button).attr('disabled', true);
 			$('.scoreCount').text(getRadioValue());
 			console.log("You got it!");
 		}
@@ -17,6 +19,7 @@ $(document).ready(function() {
 		else if (valGuess == 0) {
 			$(this).text('Incorrect');
 			$(list).css("background-color", "#e74c3c");
+			$(button).attr('disabled', true);
 			$('.scoreCount').text(getRadioValue());
 			console.log("Incorrect");
 		}
@@ -40,6 +43,8 @@ $(document).ready(function() {
 
 
 });
+
+
 
 
 
