a7767d605a9526578cec38a6081e2525b15e434d
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2014-01-16 14:09:04 +0800

    Version 1.4
     - Added final score slide

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 	var scoreCount = 0;
 
+
 	$(".submit-answer").click(function() {
 		event.preventDefault();
 		var list = $('input[name=radio-buttons]:checked').parent();
@@ -32,12 +33,28 @@ $(document).ready(function() {
 		var countText = "Score: <span class='scoreCount'>" + scoreCount + "</span> out of 10 correct";
 		$("p.scoreText").html(countText);
 
-
 	});
 
-
-
 });
+	
+	function finalScore() {
+			if (scoreCount >= 0 && scoreCount <= 4) {
+				$('p.final-score').html('Strangers on the street status: Your score of ' + scoreCount + ' out of 10 is a bit low. Try the quiz again!');
+			}
+
+			else if (scoreCount >= 5 && scoreCount <= 7) {
+				$('p.final-score').html('Acquaintance status: Not too bad, you got ' + scoreCount + ' out of 10. Try the quiz again for a high score!');
+			}
+
+			else {
+				$('p.final-score').html('BFF status: Congratulations! You got a high score of ' + scoreCount + ' out of 10.')
+			}
+		
+	}
+
+
+
+
 
 
 
