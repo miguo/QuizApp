904baa30a58fec50e08680c836547fc97b81cac5
Michelle <MichelleGuo@Michelles-MacBook-Air.local>
2014-01-18 17:06:36 +0800

    Version 1.5
     - changed CSS positioning of score count
     - change text on final slide depending on final score
     - added button to Refresh Quiz

--- js/script.js
+++ js/script.js
@@ -33,30 +33,30 @@ $(document).ready(function() {
 		var countText = "Score: <span class='scoreCount'>" + scoreCount + "</span> out of 10 correct";
 		$("p.scoreText").html(countText);
 
+		finalScore();
+
 	});
 
-});
-	
+	$('#wide-button').click(function() {
+		location.reload();
+	})
+
 	function finalScore() {
 			if (scoreCount >= 0 && scoreCount <= 4) {
-				$('p.final-score').html('Strangers on the street status: Your score of ' + scoreCount + ' out of 10 is a bit low. Try the quiz again!');
+				$('.final-feedback').html('Stranger status: Your score of ' + scoreCount + ' out of 10 is a bit low. Try the quiz again!');
 			}
 
 			else if (scoreCount >= 5 && scoreCount <= 7) {
-				$('p.final-score').html('Acquaintance status: Not too bad, you got ' + scoreCount + ' out of 10. Try the quiz again for a high score!');
+				$('.final-feedback').html('Acquaintance status: Not too bad, you got ' + scoreCount + ' out of 10. Try the quiz again for a high score!');
 			}
 
 			else {
-				$('p.final-score').html('BFF status: Congratulations! You got a high score of ' + scoreCount + ' out of 10.')
+				$('.final-feedback').html('BFF status: Congratulations! You got a high score of ' + scoreCount + ' out of 10!')
 			}
 		
 	}
 
-
-
-
-
-
+});
 
 
 
